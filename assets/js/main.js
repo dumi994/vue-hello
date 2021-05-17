7a9876fc4f3d76dcf1d73ee5791df073026a3f16
dumi994 <dumi.9944@gmail.com>
2021-05-17 15:59:53 +0200

    creato struttura e inserito saluto

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+//Descrizione: Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+//Bonus: Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+
+
+let app = new Vue(
+    {
+        el: '#root',
+        data :{
+            saluto: "Benvenuto in",
+            logoVJS:"./assets/img/Untitled-design-2019-09-03T162841.927-620x420.png",
+        },
+    }
+)
